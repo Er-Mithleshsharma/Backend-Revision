15b62d7a6cf9b2142ce5f009876ac465cecd70b7
Mithlesh Sharma <itsyour.rohan@gmail.com>
2024-12-11 23:17:20 +0530

    Database connection the right way

---
+++ src/config/Db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+
+const connectDb = async () =>{
+     mongoose.connect("mongodb+srv://itsyourrohan:gS9an1a5yR40FMfp@namastenode.dkgy2.mongodb.net/test")
+}
+
+module.exports = connectDb;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
 const express = require("express");
 const Adminauth = require("./middlewares/AdminAuth");
+const connectDb = require("./config/Db")
 const app = express();
 
-// so we need to define a middleware before all admin routes since order matters 
-
-app.use("/admin",Adminauth)
-
- app.post("/admin/signup", (req,res)=>{
-   
-    
-        // db logic
-        res.send("all data sent ");
- 
-   
- })
-
- // error middlewares  -> denfined at the last of all the code if any error is thrown in between it will catch all the errros 
-// -> takes four parameters error , request , response , next --> order matters so write carefully 
- app.use("/",(err,req,res,next)=>{
-    if(err)
-    {
-        res.status(500).send("something went wrong")
-    }
- })
-
-// If the error-handling middleware is defined at the end of the middleware stack in your Express application, and it is the last piece of middleware, you typically don’t need to call next, as there’s no subsequent middleware to pass control to.
 
 
 
-app.listen(7777,()=>{
+
+// This is how we should start the server -> we should see if the db is connected then only server should be started
+// since if server gets start earlier and anyone hit the api and db hasnt stared yet it may throw errors
+connectDb().then(()=>{
+   app.listen(7777,()=>{
     console.log("server started")
 })
- 
+}).catch((err)=>{
+    console.log(err);
+})
